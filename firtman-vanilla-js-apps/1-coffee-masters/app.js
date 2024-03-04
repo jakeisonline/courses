6c6cc167062e829c9a5cc7535a6150eb19d6b646
Jake Holman <me@jakeisonline.com>
2024-03-04 22:21:06 +0000

    Chapter 3: Routing

--- firtman-vanilla-js-apps/1-coffee-masters/app.js
+++ firtman-vanilla-js-apps/1-coffee-masters/app.js
@@ -1,10 +1,13 @@
 import Store from "./services/store.js";
 import API from "./services/api.js";
 import { loadData } from "./services/menu.js"
+import Router from "./services/router.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router
 
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
+  app.router.init();
 });
---
+++ firtman-vanilla-js-apps/1-coffee-masters/services/router.js
@@ -0,0 +1,50 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach(a => {
+      a.addEventListener("click", e => {
+        e.preventDefault();
+        Router.go(a.getAttribute("href"));
+      });
+    })
+
+    window.addEventListener("popstate", e => {
+      Router.go(e.state.route, false);
+    });
+
+    Router.go(location.pathname);
+  },
+
+  go: ( route, addToHistory=true ) => {
+    if (addToHistory) {
+      history.pushState({ route }, null, route);
+    }
+
+    let pageElement = null;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+      default:
+        if (route.startWith("/product/")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Details";
+          const paramId = route.substring(route.lastIndexOf("/")+1);
+          pageElement.dataset.id = paramId;
+        }
+    }
+
+    const cache = document.querySelector("main");
+    cache.innerHTML = "";
+    cache.appendChild(pageElement);
+    window.scrollX = 0;
+    window.scrollY = 0;
+  }
+};
+
+export default Router;
