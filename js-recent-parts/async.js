bc75c6f2400cf28e9d0c936fc761420c6ffe361e
Jake Holman <me@jakeisonline.com>
2024-03-13 23:03:43 +0000

    Async exercise

---
+++ js-recent-parts/async.js
@@ -0,0 +1,43 @@
+/* # Async
+
+We're calling some fake API calls. But they won't all come back at the same
+time, how do we handle that problem?
+*/
+
+function getFile(file) {
+  return new Promise(function (resolve) {
+    fakeAjax(file, resolve);
+  });
+}
+
+async function loadFiles(files) {
+  let requests = files.map(getFile);
+
+  for (request of requests) {
+    console.log(await request);
+  }
+
+  // for (file of files) {
+  //   const result = await getFile(file);
+  //   console.log(result);
+  // }
+}
+
+loadFiles(["file1", "file2", "file3"]);
+
+// **************************************
+
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: "The first text",
+    file2: "The middle text",
+    file3: "The last text",
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
+
+  console.log("Requesting: " + url);
+
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
+}
