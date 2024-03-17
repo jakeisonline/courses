d2b16fa27bf246351efadfad9146b7dfcf31d2bb
Jake Holman <me@jakeisonline.com>
2024-03-17 13:29:54 +0000

    Custom hooks

--- complete-react-v8/src/SearchParams.jsx
+++ complete-react-v8/src/SearchParams.jsx
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
-const BREEDS = [];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input
@@ -40,14 +60,14 @@ const SearchParams = () => {
           Breed
           <select
             id="animal"
-            disabled={BREEDS.length === 0}
+            disabled={breeds.length === 0}
             value={breed}
             onChange={(e) => {
               setBreed(e.target.value);
             }}
           >
           <option />
-          {BREEDS.map((breed) => (
+          {breeds.map((breed) => (
             <option key={breed}>{breed}</option>
           ))}
           </select>
@@ -55,7 +75,11 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
-
+      {
+        pets.map(pet => (
+          <Pet mame={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+        ))
+      }
     </div>
   );
 }
