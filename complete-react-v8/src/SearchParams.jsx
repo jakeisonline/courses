ebf4f3a41298adb8878eb603719122c6fd641b85
Jake Holman <me@jakeisonline.com>
2024-03-17 13:42:16 +0000

    Extracting components

--- complete-react-v8/src/SearchParams.jsx
+++ complete-react-v8/src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -75,11 +75,7 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
-      {
-        pets.map(pet => (
-          <Pet mame={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
-        ))
-      }
+      <Results pets={pets} />
     </div>
   );
 }
