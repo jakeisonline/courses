927aac8bada39bbbf09282dc2b08bd3045bddef8
Jake Holman <me@jakeisonline.com>
2024-03-31 21:26:13 +0100

    async with goroutines

--- go-basics/cryptomasters/main.go
+++ go-basics/cryptomasters/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"jakeisonline.com/go/cryptomasters/api"
 )
 
 func main() {
-	rate, err := api.GetRate("BTC")
+	currencies := []string{"BTC", "BCH", "ETH"}
+	var wg sync.WaitGroup
+
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func() {
+			getCurrencyData(currency)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
 	if err == nil {
 		fmt.Printf("Rate for %v is %.2f\n", rate.Currency, rate.Price)
 	}
