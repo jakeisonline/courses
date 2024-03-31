5698a7a95889b805df13c0217e1a66ae055f8aa7
Jake Holman <me@jakeisonline.com>
2024-03-31 21:32:22 +0100

    testing in go

--- go-basics/cryptomasters/api/cex.go
+++ go-basics/cryptomasters/api/cex.go
@@ -13,6 +13,9 @@ import (
 const apiUrl = "https://cex.io/api/ticker/%s/USD"
 
 func GetRate(currency string) (*datatypes.Rate, error) {
+	if len(currency) != 3 {
+		return nil, fmt.Errorf("3 characters required, but %d received", len(currency))
+	}
 	upCurrency := strings.ToUpper(currency)
 	resp, err := http.Get(fmt.Sprintf(apiUrl, upCurrency))
 	if err != nil {
