458dd9b5b49caa8af100c0bf2a8846970cef4d80
Jake Holman <me@jakeisonline.com>
2024-04-10 22:00:26 +0100

    get actual todo

--- next-js-v3/app/api/todo/route.ts
+++ next-js-v3/app/api/todo/route.ts
@@ -2,7 +2,8 @@ import db from '@/utils/db'
 import { NextResponse } from "next/server"
 
 export const GET = async (request: Request) => {
-  return NextResponse.json({ message: "hello get" })
+  const todos = await db.todo.findMany({})
+  return NextResponse.json({ data: todos })
 }
 
 export const POST = async (request: Request) => {
