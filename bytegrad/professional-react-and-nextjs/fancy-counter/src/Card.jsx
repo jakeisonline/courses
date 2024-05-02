4f21d0c0d70bc109e1fcc27e261aa00655781c6c
Jake Holman <me@jakeisonline.com>
2024-05-02 21:06:25 +0100

    styling

--- bytegrad/professional-react-and-nextjs/fancy-counter/src/Card.jsx
+++ bytegrad/professional-react-and-nextjs/fancy-counter/src/Card.jsx
@@ -2,14 +2,15 @@ import Count from "./Count"
 import CountButtons from "./CountButtons"
 import ResetButton from "./ResetButton"
 import Title from "./Title"
+import "./index.css"
 
 export default function Card() {
   return (
-    <>
+    <div className="card">
       <Title />
       <Count />
       <ResetButton />
       <CountButtons />
-    </>
+    </div>
   )
 }
