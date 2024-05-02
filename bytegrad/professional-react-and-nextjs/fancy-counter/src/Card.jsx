2f70479f051a261672237bfa569ebe1688ca4a84
Jake Holman <me@jakeisonline.com>
2024-05-02 22:00:26 +0100

    keyboard interaction

--- bytegrad/professional-react-and-nextjs/fancy-counter/src/Card.jsx
+++ bytegrad/professional-react-and-nextjs/fancy-counter/src/Card.jsx
@@ -4,10 +4,27 @@ import ResetButton from "./ResetButton"
 import Title from "./Title"
 import "./index.css"
 import { useState } from "react"
+import { useEffect } from "react"
 
 export default function Card() {
   const [count, setCount] = useState(0)
   const locked = count === 5 ? true : false
+
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      if (locked) return
+      if (e.code === "Space") {
+        setCount(count + 1)
+      }
+      e.preventDefault()
+    }
+
+    window.addEventListener("keydown", handleKeydown)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown)
+    }
+  }, [count, locked])
 
   return (
     <div className={`card ${locked ? "card--limit" : ""}`}>
