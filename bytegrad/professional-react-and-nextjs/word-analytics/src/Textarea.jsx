9cf0d05cff1fc589f1e7af8a1ba4a0a0555e5d71
Jake Holman <me@jakeisonline.com>
2024-05-03 21:47:14 +0100

    textarea interactions

--- bytegrad/professional-react-and-nextjs/word-analytics/src/Textarea.jsx
+++ bytegrad/professional-react-and-nextjs/word-analytics/src/Textarea.jsx
@@ -1,3 +1,34 @@
+import { useState } from "react"
+import Warning from "./Warning"
+
 export default function Textarea() {
-  return <textarea className="textarea" />
+  const [text, setText] = useState("")
+  const [warningText, setWarningText] = useState("")
+
+  const handleChange = (e) => {
+    let newText = e.target.value
+    if (newText.includes("<script>")) {
+      newText = newText.replace("<script>", "")
+      setWarningText("Scripts are not allowed")
+    } else if (newText.includes("@")) {
+      newText = newText.replace("@", "")
+      setWarningText("Don't @ me")
+    } else {
+      setWarningText("")
+    }
+
+    setText(newText)
+  }
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text..."
+        spellCheck={false}
+      />
+      {warningText && <Warning warningText={warningText} />}
+    </div>
+  )
 }
