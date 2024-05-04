087718130915b3e940402696948ed0551bc500ec
Jake Holman <me@jakeisonline.com>
2024-05-04 11:04:44 +0100

    refactor buttons

--- bytegrad/professional-react-and-nextjs/trekbag/src/components/ButtonGroup.jsx
+++ bytegrad/professional-react-and-nextjs/trekbag/src/components/ButtonGroup.jsx
@@ -1,12 +1,22 @@
 import Button from "./Button"
 
 export default function ButtonGroup() {
+  const buttonLabels = [
+    "Mark all as complete",
+    "Mark all as incomplete",
+    "Reset to initial",
+    "Remove all items",
+  ]
+
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      {buttonLabels.map((label) => {
+        return (
+          <Button key={label} type="secondary">
+            {label}
+          </Button>
+        )
+      })}
     </section>
   )
 }
