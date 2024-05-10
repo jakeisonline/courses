39703c6db3268fa61dbf1923440eee2277ba3bb6
Jake Holman <me@jakeisonline.com>
2024-05-10 22:38:16 +0100

    portal the popover

--- bytegrad/professional-react-and-nextjs/rmtdev/src/components/BookmarksPopover.tsx
+++ bytegrad/professional-react-and-nextjs/rmtdev/src/components/BookmarksPopover.tsx
@@ -1,6 +1,7 @@
 import { forwardRef } from "react"
 import { useBookmarksContext } from "../lib/hooks"
 import JobList from "./JobList"
+import { createPortal } from "react-dom"
 
 type BookmarksPopoverProps = {
   ref: React.RefObject<HTMLDivElement>
@@ -10,10 +11,11 @@ const BookmarksPopover = forwardRef<HTMLDivElement, BookmarksPopoverProps>(
   function (_, ref) {
     const { bookmarkedJobItems, isBookmarksLoading } = useBookmarksContext()
 
-    return (
+    return createPortal(
       <div ref={ref} className="bookmarks-popover">
         <JobList jobItems={bookmarkedJobItems} isLoading={isBookmarksLoading} />
-      </div>
+      </div>,
+      document.body,
     )
   },
 )
