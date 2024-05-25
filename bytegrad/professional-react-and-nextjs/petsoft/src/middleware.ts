ac1404f27312922cbc4d186c3c19588db3ea2651
Jake Holman <me@jakeisonline.com>
2024-05-25 21:12:16 +0100

    introduce middleware and add user table, seed data

---
+++ bytegrad/professional-react-and-nextjs/petsoft/src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server"
+
+export function middleware(request: Request) {
+  console.log(request.url)
+  return NextResponse.next()
+}
+
+export const config = {
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
