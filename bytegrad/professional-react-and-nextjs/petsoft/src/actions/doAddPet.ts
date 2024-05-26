9ad3b5fdfff20df5ad991be3e9cc0ed61174a91b
Jake Holman <me@jakeisonline.com>
2024-05-26 12:44:25 +0100

    scope pet creation to users

--- bytegrad/professional-react-and-nextjs/petsoft/src/actions/doAddPet.ts
+++ bytegrad/professional-react-and-nextjs/petsoft/src/actions/doAddPet.ts
@@ -1,12 +1,19 @@
 "use server"
 
 import prisma from "@/lib/db"
-import { TErrorMutatePet, TMutatingPet, TPromisePet } from "@/lib/types"
+import { TErrorMutatePet, TPromisePet } from "@/lib/types"
 import { petFormSchema } from "@/lib/validations"
+import { auth } from "@/lib/auth"
 import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 
 export async function addPet(petData: unknown): Promise<TPromisePet> {
   let error: TErrorMutatePet, response
+
+  const userSession = await auth()
+  if (!userSession?.user) {
+    redirect("/login")
+  }
 
   // Run validations
   const validatedPetData = petFormSchema.safeParse(petData)
@@ -19,7 +26,14 @@ export async function addPet(petData: unknown): Promise<TPromisePet> {
 
   try {
     response = await prisma.pet.create({
-      data: validatedPetData.data,
+      data: {
+        ...validatedPetData.data,
+        user: {
+          connect: {
+            id: userSession.user.id,
+          },
+        },
+      },
     })
   } catch (error) {
     error = { message: `Failed to add pet to database: \n\n${error}` }
--- bytegrad/professional-react-and-nextjs/petsoft/src/lib/types.ts
+++ bytegrad/professional-react-and-nextjs/petsoft/src/lib/types.ts
@@ -2,10 +2,7 @@ import { Pet } from "@prisma/client"
 
 export type TPet = Pet
 
-export type TMutatingPet = Omit<
-  TPet,
-  "id" | "userId" | "createdAt" | "updatedAt"
->
+export type TMutatingPet = Omit<TPet, "id" | "createdAt" | "updatedAt">
 
 export type TErrorMutatePet =
   | {
