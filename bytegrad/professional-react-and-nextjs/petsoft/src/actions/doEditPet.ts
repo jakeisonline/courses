bca0f36537c95d1dbd6dad669b5921f1ea0237fb
Jake Holman <me@jakeisonline.com>
2024-05-26 13:04:36 +0100

    scope pet edits to only allow edits when user owns pet

--- bytegrad/professional-react-and-nextjs/petsoft/src/actions/doEditPet.ts
+++ bytegrad/professional-react-and-nextjs/petsoft/src/actions/doEditPet.ts
@@ -1,9 +1,11 @@
 "use server"
 
+import { auth } from "@/lib/auth"
 import prisma from "@/lib/db"
 import { TErrorMutatePet, TMutatingPet, TPromisePet } from "@/lib/types"
 import { petFormSchema, petIdSchema } from "@/lib/validations"
 import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 
 export async function editPet(
   petId: unknown,
@@ -11,12 +13,31 @@ export async function editPet(
 ): Promise<TPromisePet> {
   let error: TErrorMutatePet, response
 
+  // Auth check
+  const userSession = await auth()
+  if (!userSession?.user) {
+    redirect("/login")
+  }
+
   // Run validations
   const validatedPetId = petIdSchema.safeParse(petId)
   const validatedPetData = petFormSchema.safeParse(petData)
   if (!validatedPetData.success || !validatedPetId.success) {
     error = {
       message: `Failed to validate pet data when editing pet`,
+    }
+    return { error, response }
+  }
+
+  // Allowed check (user owns pet)
+  const petOwnerId = await prisma.pet.findUnique({
+    where: { id: validatedPetId.data },
+    select: { userId: true },
+  })
+
+  if (!petOwnerId || petOwnerId.userId !== userSession.user.id) {
+    error = {
+      message: `User does not own pet (Pet ID: ${validatedPetId.data})`,
     }
     return { error, response }
   }
