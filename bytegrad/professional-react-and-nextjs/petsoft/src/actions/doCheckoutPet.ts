002e38ac62de8ff40376455e62ea9c75afbb94a7
Jake Holman <me@jakeisonline.com>
2024-05-26 13:01:38 +0100

    scope pet deletion (checkout) to only pets the user owns

--- bytegrad/professional-react-and-nextjs/petsoft/src/actions/doCheckoutPet.ts
+++ bytegrad/professional-react-and-nextjs/petsoft/src/actions/doCheckoutPet.ts
@@ -1,12 +1,20 @@
 "use server"
 
+import { auth } from "@/lib/auth"
 import prisma from "@/lib/db"
 import { TErrorMutatePet } from "@/lib/types"
 import { petIdSchema } from "@/lib/validations"
 import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 
 export async function checkoutPet(petId: unknown) {
   let error: TErrorMutatePet, response
+
+  // Auth check
+  const userSession = await auth()
+  if (!userSession?.user) {
+    redirect("/login")
+  }
 
   // Run validations
   const validatedPetId = petIdSchema.safeParse(petId)
@@ -14,6 +22,19 @@ export async function checkoutPet(petId: unknown) {
   if (!validatedPetId.success) {
     error = {
       message: `Failed to validate pet id when checking out pet`,
+    }
+    return { error, response }
+  }
+
+  // Allowed check (user owns pet)
+  const petOwnerId = await prisma.pet.findUnique({
+    where: { id: validatedPetId.data },
+    select: { userId: true },
+  })
+
+  if (!petOwnerId || petOwnerId.userId !== userSession.user.id) {
+    error = {
+      message: `User does not own pet (Pet ID: ${validatedPetId.data})`,
     }
     return { error, response }
   }
