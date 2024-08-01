2eb8421eeff8a7e74d431543cd70377575bade1a
Jake Holman <me@jakeisonline.com>
2024-08-01 21:48:32 +0100

    create a Product data model, and use it to render a product card

--- frontendmasters/flutter/coffee_masters/lib/pages/menu.dart
+++ frontendmasters/flutter/coffee_masters/lib/pages/menu.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_masters/datamodel.dart';
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
@@ -5,6 +6,44 @@ class MenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var p = Product(
+      id: 1,
+      name: "Espresso",
+      price: 2.5,
+      image: "1",
+    );
+    return ProductItem(
+      product: p,
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("images/black_coffee.png"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(product.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("\$${product.price}"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
