c4a5b0a5bf2979e563aaa8f4b56d691948bd2c77
Jake Holman <me@jakeisonline.com>
2024-08-30 11:49:07 +0100

    adds further tailwind config

--- nan.fyi/svg/tailwind.config.mjs
+++ nan.fyi/svg/tailwind.config.mjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from "tailwindcss/plugin"
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+        "border": "hsl(var(--border))",
+        "input": "hsl(var(--input))",
+        "ring": "hsl(var(--ring))",
+        "link": "hsl(var(--link))",
+        "link-hover": "hsl(var(--link-hover))",
+        "background": "hsl(var(--background))",
+        "foreground": "hsl(var(--foreground))",
+        "primary": {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        "secondary": {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+      },
+		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addBase }) {
+      addBase({
+        html: { fontSize: "22px" },
+      })
+    }),
+	],
 }
