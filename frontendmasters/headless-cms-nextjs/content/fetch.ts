166509e06044af733bdf237976fa1fab2cb47b7b
Jake Holman <me@jakeisonline.com>
2024-09-21 14:45:05 +0100

    initial contentful project set up

---
+++ frontendmasters/headless-cms-nextjs/content/fetch.ts
@@ -0,0 +1,12 @@
+export const contentGqlFetcher = async (query: string, variables = {}, preview = false) => {
+  const response = await
+    fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": preview ? `Bearer ${process.env.CONTENTFUL_PREVIEW_TOKEN}` : `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify({query, variables}),
+    })
+}
