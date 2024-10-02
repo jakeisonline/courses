129fc7691549d4c96eac9c6b14e7fb3ca2b047c1
Jake Holman <me@jakeisonline.com>
2024-10-02 19:13:36 +0100

    update readme path

--- _scripts/update-readme.ts
+++ _scripts/update-readme.ts
@@ -3,7 +3,7 @@ import mustache from "mustache"
 import getCourses from "@/utils/get-courses"
 import getStats from "@/utils/get-stats"
 
-const readme = fs.readFileSync("./_scripts/readme.md", "utf8").toString()
+const readme = fs.readFileSync("./_scripts/readme.mustache", "utf8").toString()
 
 const context = {
   stats: getStats(),
@@ -13,5 +13,3 @@ const context = {
 const output = mustache.render(readme, context)
 
 fs.writeFileSync("./_scripts/_readme.md", output)
-
-//console.log(output)
