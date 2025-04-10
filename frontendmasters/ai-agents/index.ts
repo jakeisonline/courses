911e4163633f12888f131ec903891d509719290f
Jake Holman <me@jakeisonline.com>
2025-04-10 20:59:24 +0100

    initial ai comms

--- frontendmasters/ai-agents/index.ts
+++ frontendmasters/ai-agents/index.ts
@@ -1,8 +1,13 @@
-import 'dotenv/config'
+import "dotenv/config"
+import { runLLM } from "@/llm.ts"
 
 const userMessage = process.argv[2]
 
 if (!userMessage) {
-  console.error('Please provide a message')
+  console.error("Please provide a message")
   process.exit(1)
 }
+
+const response = await runLLM({ userMessage })
+
+console.log(response)
